fe20a4acaa59bd4b22efc466ef7857eaed24b4a9
TomasCandeloM <tomascamo@unisabana.edu.co>
2023-09-05 19:56:40 -0500

    Agrega funciones de logica y controller de perfil

--- src/main/java/co/edu/unisabana/recursos_humanos/controlador/GestionPerfilController.java
+++ src/main/java/co/edu/unisabana/recursos_humanos/controlador/GestionPerfilController.java
@@ -1,11 +1,13 @@
 package co.edu.unisabana.recursos_humanos.controlador;
 
+import co.edu.unisabana.recursos_humanos.controlador.dto.PerfilEmpleadoDTO;
 import co.edu.unisabana.recursos_humanos.controlador.dto.Respuesta;
+import co.edu.unisabana.recursos_humanos.db.entidad.EmpleadoDB;
 import co.edu.unisabana.recursos_humanos.db.entidad.PerfilEmpleadoDB;
 import co.edu.unisabana.recursos_humanos.logica.LogicaPerfil;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class GestionPerfilController {
@@ -16,13 +18,46 @@ public class GestionPerfilController {
         this.logica = logica;
     }
 
+    @GetMapping(path = "/perfil/buscar/todos")
+    public List<PerfilEmpleadoDB> buscarPerfiles() {
+        return logica.buscarPerfilTodos();
+    }
+
+    @GetMapping(path = "/perfil/buscar/id")
+    public String buscarPerfilPorID(@RequestParam int id) {
+        return logica.buscarPerfilPorID(id);
+    }
+
+
+
     @PostMapping(path = "/perfil/subir")
-    public Respuesta subirRol(@RequestBody PerfilEmpleadoDB perfil) {
+    public Respuesta subirRol(@RequestBody PerfilEmpleadoDTO perfil) {
         try {
             logica.crearPerfil(perfil);
             return new Respuesta("Exitoso","El perfil se ha creado correctamente.");
         } catch (Exception e) {
             return new Respuesta("Fallido", "Algo ha salido mal. No se ha podido crear el perfil");
         }
+    }
+
+    @PutMapping(path = "/perfil/actualizar")
+    public Respuesta actualizarPerfil (@RequestBody PerfilEmpleadoDTO perfilNuevo){
+        try {
+            logica.actualizarPerfil(perfilNuevo.getId(), perfilNuevo);
+            return new Respuesta("Exitoso", "El perfil ha sido eliminado correctamente.");
+        } catch (Exception e){
+            return new Respuesta("Fallido","Algo ha salido mal, no se ha podido eliminar el perfil.");
+        }
+    }
+
+    @DeleteMapping (path = "/perfil/eliminar")
+    public Respuesta eliminarPerfil (@RequestParam int id){
+        try {
+            logica.eliminarPerfil(id);
+            return new Respuesta("Exitoso", "El perfil ha sido eliminado correctamente.");
+        } catch (Exception e){
+            return new Respuesta("Fallido","Algo ha salido mal, no se ha podido eliminar el perfil.");
+        }
+
     }
 }
--- src/main/java/co/edu/unisabana/recursos_humanos/logica/LogicaPerfil.java
+++ src/main/java/co/edu/unisabana/recursos_humanos/logica/LogicaPerfil.java
@@ -1,10 +1,14 @@
 package co.edu.unisabana.recursos_humanos.logica;
 
+import co.edu.unisabana.recursos_humanos.controlador.dto.PerfilEmpleadoDTO;
+import co.edu.unisabana.recursos_humanos.db.entidad.EmpleadoDB;
 import co.edu.unisabana.recursos_humanos.db.entidad.PerfilEmpleadoDB;
 import co.edu.unisabana.recursos_humanos.db.PerfilEmpleadoRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LogicaPerfil {
@@ -14,9 +18,40 @@ public class LogicaPerfil {
         this.perfilEmpleadoRepository = perfilEmpleadoRepository;
     }
 
-    public void crearPerfil(PerfilEmpleadoDB perfil){
+    public List<PerfilEmpleadoDB> buscarPerfilTodos() {
+        return perfilEmpleadoRepository.findAll();
+    }
+
+    public String buscarPerfilPorID(int id) {
+        Optional<PerfilEmpleadoDB> perfil = perfilEmpleadoRepository.findById(id);
+        if(perfil.isPresent()){
+            return perfil.toString();
+        } else {
+            return "El usuario no existe.";
+        }
+    }
+
+    public void crearPerfil(PerfilEmpleadoDTO perfilEmpleadoDTO){
+        PerfilEmpleadoDB perfil = new PerfilEmpleadoDB();
+        perfil.setId(perfilEmpleadoDTO.getId());
+        perfil.setIdEmpleado(perfilEmpleadoDTO.getIdEmpleado());
+        perfil.setHabilidades(perfilEmpleadoDTO.getHabilidades());
+        perfil.setAnosExperiencia(perfilEmpleadoDTO.getAnosExpereiencia());
         perfil.setFechaActualizacion(LocalDateTime.now());
         perfil.setFechaCreacion(LocalDateTime.now());
         perfilEmpleadoRepository.save(perfil);
+    }
+
+    public void actualizarPerfil (int id, PerfilEmpleadoDTO nuevoPerfil){
+        PerfilEmpleadoDB actualizarPerfilEmpleado = perfilEmpleadoRepository.getReferenceById(id);
+        actualizarPerfilEmpleado.setIdEmpleado(nuevoPerfil.getIdEmpleado());
+        actualizarPerfilEmpleado.setHabilidades(nuevoPerfil.getHabilidades());
+        actualizarPerfilEmpleado.setAnosExperiencia(nuevoPerfil.getAnosExpereiencia());
+        actualizarPerfilEmpleado.setFechaActualizacion(LocalDateTime.now());
+        perfilEmpleadoRepository.save(actualizarPerfilEmpleado);
+    }
+
+    public void eliminarPerfil (int id){
+        perfilEmpleadoRepository.deleteById(id);
     }
 }
