aeec2704d8bbc140cbe359e74c8d7a533ad9121f
JuanJosÃeCifuentes <jcifuentesc03@gmail.com>
2023-11-18 12:51:11 -0500

    Permite basic authentication con CORS

--- src/main/java/co/edu/unisabana/recursos_humanos/config/WebSecurityConfig.java
+++ src/main/java/co/edu/unisabana/recursos_humanos/config/WebSecurityConfig.java
@@ -1,9 +1,12 @@
 package co.edu.unisabana.recursos_humanos.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -11,9 +14,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
+        http.csrf().disable()
+                .cors().and()
+                .authorizeRequests()
+                .antMatchers("/**").authenticated() // Secure this endpoint
+                .anyRequest().permitAll()
                 .and()
-                .authorizeRequests().antMatchers("*").permitAll();
+                .httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
 
